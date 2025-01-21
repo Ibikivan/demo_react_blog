46e59188948c7f44a7006a33b1b1e0c3bc3163e4
Roche Ngaleu <ibikivan1@gmail.com>
2025-01-21 19:47:07 +0100

    Ajout du proxy pour contourner les problème de CORS

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+      host: true, // Écoute sur toutes les interfaces réseau
+      port: 3000,  // (Optionnel) Change le port si nécessaire
+      proxy: {
+        '/api': {
+        target: 'https://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  }
 })
